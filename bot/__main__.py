b6d0e71bf55e20f51e1fd987c639cf35b643f352
Pratikllp <118655982+Pratikllp@users.noreply.github.com>
2023-02-22 12:53:50 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,7 +4,7 @@ from .utubebot import UtubeBot
 from .config import Config
 
 
-if __name__ == "__main__":
+if utubebot == "__main__":
     logging.basicConfig(level=logging.DEBUG if Config.DEBUG else logging.INFO)
     logging.getLogger("pyrogram").setLevel(
         logging.INFO if Config.DEBUG else logging.WARNING
